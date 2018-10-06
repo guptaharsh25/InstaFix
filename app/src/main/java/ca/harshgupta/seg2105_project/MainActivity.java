80d8f3f51194290b53732074656b1d8f16ac3f3d
guptaharsh25 <reyharsh25@gmail.com>
2018-10-06 19:13:49 -0400

    finished implementing MainActivity

--- app/src/main/java/ca/harshgupta/seg2105_project/MainActivity.java
+++ app/src/main/java/ca/harshgupta/seg2105_project/MainActivity.java
@@ -1,7 +1,11 @@
 package ca.harshgupta.seg2105_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.common.oob.SignUp;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onSignIn(View view){
+        Intent intentToSignIn = new Intent(getApplicationContext(), SignIn.class);
+        startActivityForResult(intentToSignIn,0);
+    }
+
+    public void onSignUp(View view){
+        Intent intentToSignUp = new Intent(getApplicationContext(), SignUp.class);
+        startActivityForResult(intentToSignUp, 0);
+    }
 
 }
