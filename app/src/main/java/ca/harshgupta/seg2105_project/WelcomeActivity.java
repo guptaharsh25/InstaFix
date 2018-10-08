12b02ecf1f79918d2b05514dd1a9e05b20f042b4
Bhalachandra Malghan <bmalg082@uottawa.ca>
2018-10-07 23:49:00 -0400

    signout functionality completed (in Welcome Activity)

--- app/src/main/java/ca/harshgupta/seg2105_project/WelcomeActivity.java
+++ app/src/main/java/ca/harshgupta/seg2105_project/WelcomeActivity.java
@@ -1,7 +1,11 @@
 package ca.harshgupta.seg2105_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void onSignOut (View view){
+        FirebaseAuth.getInstance().signOut();
+        Intent intentToSignOut = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(intentToSignOut,0);
     }
 }
