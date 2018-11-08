cc62c8bcb4d5c6064bea4d16500f4e2929e1b289
Nischal098 <nshar082@uottawa.ca>
2018-11-08 16:16:37 -0500

    Made the Service custom Adapater take input from the firebase

--- app/src/main/java/ca/harshgupta/seg2105_project/ServiceCustomAdapter.java
+++ app/src/main/java/ca/harshgupta/seg2105_project/ServiceCustomAdapter.java
@@ -1,6 +1,7 @@
 package ca.harshgupta.seg2105_project;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,12 +9,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ServiceCustomAdapter extends ArrayAdapter{
     private final Context context;
-    private final String[] myKeys;
+    private String[] myKeys;
 
     public ServiceCustomAdapter(Context context, String[] serviceList){
         super(context, R.layout.service_layout, serviceList);
@@ -26,16 +30,20 @@ public class ServiceCustomAdapter extends ArrayAdapter{
         View rowView = inflater.inflate(R.layout.service_layout, parent, false);
 
         TextView serviceNameText = (TextView) rowView.findViewById(R.id.serviceName);
-        TextView serviceRateText = (TextView) rowView.findViewById(R.id.serviceRate);
+        final TextView serviceRateText = (TextView) rowView.findViewById(R.id.serviceRate);
 
-        String serviceName = FirebaseDatabase.getInstance().getReference().child("Accounts")
-                .child(myKeys[position]).child("name").toString();
-        Double serviceRate = Double.parseDouble(FirebaseDatabase.getInstance().getReference().child("Services").child(myKeys[position]).child("rate").toString());
+        DatabaseReference mServices = FirebaseDatabase.getInstance().getReference().child("Services");
 
-        serviceNameText.setText(serviceName);
+        serviceNameText.setText(myKeys[position]);
 
-        serviceRateText.setText(serviceRate.toString());
-
+        mServices.child(myKeys[position]).child("rate").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                serviceRateText.setText(dataSnapshot.getValue(Integer.class));
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
         return rowView;
     }
 }
