56c949021b2442208162379ecf4803bf1c0d2d7c
guptaharsh25 <reyharsh25@gmail.com>
2018-11-08 21:19:24 -0500

    fix merge error

--- app/src/main/java/ca/harshgupta/seg2105_project/ServiceCustomAdapter.java
+++ app/src/main/java/ca/harshgupta/seg2105_project/ServiceCustomAdapter.java
@@ -11,11 +11,13 @@ import android.widget.TextView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.w3c.dom.Node;
+
 public class ServiceCustomAdapter extends ArrayAdapter{
     private final Context context;
     private final String[] myKeys;
 
-    public ServiceCustomAdapter(Context context, String[] serviceList){
+    public ServiceCustomAdapter(Context context, String[] serviceList, Node[] keys){
         super(context, R.layout.service_layout, serviceList);
         this.context = context;
         this.myKeys = serviceList;
