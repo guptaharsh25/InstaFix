551f5e160bd13322ffb6fe3c3cb03ec9a5329d27
Haard Trivedi <trivedi.haard99@gmail.com>
2018-11-14 18:27:25 -0500

    Modfied Firebase helper class

--- app/src/main/java/ca/harshgupta/seg2105_project/Firebase.java
+++ app/src/main/java/ca/harshgupta/seg2105_project/Firebase.java
@@ -7,8 +7,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
-
 public class Firebase {
 
     public DatabaseReference getMRootRef(){
