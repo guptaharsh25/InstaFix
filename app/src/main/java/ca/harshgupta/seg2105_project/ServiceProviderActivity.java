63cd7d08eb9aba75d0c7bc255e0d1e780226fd82
Haard Trivedi <trivedi.haard99@gmail.com>
2018-11-18 06:30:57 -0500

    Added add and remove service feature for Service Providers. Option to add/remove display in a dialog box when a service from the list is clicked on. If service is already added then it will not add again. Toasts after each add or remove button click to inform user

--- app/src/main/java/ca/harshgupta/seg2105_project/ServiceProviderActivity.java
+++ app/src/main/java/ca/harshgupta/seg2105_project/ServiceProviderActivity.java
@@ -1,11 +1,15 @@
 package ca.harshgupta.seg2105_project;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.Activity;
 import android.os.Handler;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -33,6 +37,7 @@ public class ServiceProviderActivity extends Activity {
     private DatabaseReference mUserRef;
 
     private String[] keys;
+    private String[] allKeys;
 
     private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -43,6 +48,7 @@ public class ServiceProviderActivity extends Activity {
     private TextView listTypeText;
 
     private int listSet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,10 +65,11 @@ public class ServiceProviderActivity extends Activity {
         mRootRef = FirebaseDatabase.getInstance().getReference();
         mServicesRef = mRootRef.child("Services");
         mUserRef = mRootRef.child("Accounts").child(user.getUid());
-        //Toast.makeText(ServiceProviderActivity.this,mUserRef.child("ProvidedServices").getKey(),Toast.LENGTH_LONG).show();
 
         setKeys(1);
         setAdapterServices();
+        removeServices();
+
     }
 
     private void instantiateKeys(DatabaseReference reference){
@@ -75,6 +82,24 @@ public class ServiceProviderActivity extends Activity {
                     keys[i] = postSnapShot.getKey();
                     i++;
                 }
+                removeServices();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
+        });
+    }
+
+    private void instantiateAllKeys(){
+        mServicesRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int i = 0;
+                allKeys = new String[(int) dataSnapshot.getChildrenCount()];
+                for (DataSnapshot postSnapShot : dataSnapshot.getChildren()) {
+                    allKeys[i] = postSnapShot.getKey();
+                    i++;
+                }
+                updateServicesList();
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {}
@@ -85,7 +110,11 @@ public class ServiceProviderActivity extends Activity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                serviceAdapter = new ServiceCustomAdapter(ServiceProviderActivity.this, keys);
+                if(listSet==0){
+                    serviceAdapter = new ServiceCustomAdapter(ServiceProviderActivity.this, allKeys);
+                } else{
+                    serviceAdapter = new ServiceCustomAdapter(ServiceProviderActivity.this, keys);
+                }
                 serviceSPList = (ListView) findViewById(R.id.listSPServices);
                 serviceSPList.setAdapter(serviceAdapter);
                 serviceAdapter.notifyDataSetChanged();
@@ -95,8 +124,9 @@ public class ServiceProviderActivity extends Activity {
 
     public void setKeys(int type){
         //type = 0 all services, type = 1 provided services
+        this.listSet = type;
         if(type == 0){
-            instantiateKeys(mServicesRef);
+            instantiateAllKeys();
         }else if(type == 1){
             instantiateKeys(mUserRef.child("ProvidedServices"));
         }
@@ -107,16 +137,135 @@ public class ServiceProviderActivity extends Activity {
             setKeys(0);
             setAdapterServices();
             addServices.setText("Back");
-            listSet = 0;
             listTypeText.setText("Click the service to add: ");
+            updateServicesList();
         }else if(listSet == 0){
             setKeys(1);
             setAdapterServices();
             addServices.setText("Add");
-            listSet = 1;
             listTypeText.setText("Your Services: ");
+            removeServices();
         }
-
     }
 
+    public void updateServicesList(){
+        if(allKeys!=null){
+            serviceAdapter = new ServiceCustomAdapter(ServiceProviderActivity.this, allKeys);
+            serviceSPList = (ListView) findViewById(R.id.listSPServices);
+            serviceSPList.setAdapter(serviceAdapter);
+            serviceAdapter.notifyDataSetChanged();
+            final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            serviceSPList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    final String selectedListItem = ((TextView) view.findViewById(R.id.serviceName)).getText().toString();
+                    for(final String key: allKeys){
+                        mServicesRef.child(key).child("name").addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.getValue()!=null && dataSnapshot.getValue(String.class).equals(selectedListItem)){
+                                    final String selectedItemKey = key;
+                                    final AlertDialog alertDialog = new AlertDialog.Builder(ServiceProviderActivity.this).create();
+                                    alertDialog.setTitle("Add Service");
+                                    alertDialog.setMessage("Would you like to do with the service: " + selectedListItem);
+                                    alertDialog.setButton(Dialog.BUTTON_POSITIVE,"Add", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            addServiceKey(selectedItemKey);
+                                        }
+                                    });
+                                    alertDialog.setButton(Dialog.BUTTON_NEGATIVE,"Cancel", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.cancel();
+                                        }
+                                    });
+                                    if(listSet==0){
+                                        alertDialog.show();
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    }
+
+    public void addServiceKey(String itemKey){
+        for(String key: keys){
+            if(itemKey.equals(key)){
+                Toast.makeText(ServiceProviderActivity.this,"Service Already Provided",Toast.LENGTH_LONG).show();
+                return;
+            }
+        }
+        mUserRef.child("ProvidedServices").child(itemKey).setValue(1);
+        setKeys(0);
+        Toast.makeText(ServiceProviderActivity.this,"Service Added",Toast.LENGTH_LONG).show();
+    }
+
+    public void removeServices() {
+        if(keys!=null){
+            serviceAdapter = new ServiceCustomAdapter(ServiceProviderActivity.this, keys);
+            serviceSPList = (ListView) findViewById(R.id.listSPServices);
+            serviceSPList.setAdapter(serviceAdapter);
+            serviceAdapter.notifyDataSetChanged();
+            final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            serviceSPList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    final String selectedListItem = ((TextView) view.findViewById(R.id.serviceName)).getText().toString();
+                    for(final String key: keys){
+                        mServicesRef.child(key).child("name").addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.getValue()!=null && dataSnapshot.getValue(String.class).equals(selectedListItem)){
+                                    final String selectedItemKey = key;
+                                    final AlertDialog alertDialog = new AlertDialog.Builder(ServiceProviderActivity.this).create();
+                                    alertDialog.setTitle("Remove Service");
+                                    alertDialog.setMessage("Would you like to do with the service: " + selectedListItem);
+                                    alertDialog.setButton(Dialog.BUTTON_POSITIVE,"Remove", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            removeServiceKey(selectedItemKey);
+                                        }
+                                    });
+                                    alertDialog.setButton(Dialog.BUTTON_NEGATIVE,"Cancel", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.cancel();
+                                        }
+                                    });
+                                    if(listSet==1){
+                                        alertDialog.show();
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    }
+
+    public void removeServiceKey(String itemKey){
+        for(String key: keys){
+            if(itemKey.equals(key)){
+                mUserRef.child("ProvidedServices").child(itemKey).removeValue();
+                setKeys(1);
+                setAdapterServices();
+                Toast.makeText(ServiceProviderActivity.this,"Service Removed",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
